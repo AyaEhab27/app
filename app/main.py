2a7dfb7c632260dbe0e919caf6880b390f4e5197
Aya Ehab <xxxeeee2468@gmail.com>
2025-03-10 07:29:12 +0200

    main.py

--- app/main.py
+++ app/main.py
@@ -10,16 +10,8 @@ import time
 import base64
 from gtts import gTTS
 from fastapi.middleware.cors import CORSMiddleware
-import firebase_admin
-from firebase_admin import credentials, storage
 
-firebase_config_path = os.path.join(os.path.dirname(__file__), 'config', 'sound-6893c-firebase-adminsdk-fbsvc-8104119578.json')
-cred = credentials.Certificate(firebase_config_path)
-firebase_admin.initialize_app(cred, {
-    'storageBucket': 'sound-6893c.appspot.com'  
-})
-
-# start API
+# Start API
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -28,8 +20,7 @@ app.add_middleware(
     allow_methods=["*"],  
     allow_headers=["*"],  
 )
-
-# models
+# model paths
 model_paths = {
     "arabic_letters": "app/models/Aalpha2_sign_language_model.h5",
     "arabic_numbers": "app/models/AN2_sign_language_model.h5",
@@ -37,18 +28,21 @@ model_paths = {
     "english_numbers": "app/models/EN_sign_language_model.h5"
 }
 
+#  load models
 models = {}
 for key, path in model_paths.items():
     models[key] = tf.keras.models.load_model(path)
     models[key].compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
+# categories
 categories = {
-    "arabic_letters": ["ain", "al", "alef", "beh", "dad", "dal", "feh", "ghain", "hadhf", "hah", "heh", "jeem", "kaf", "khah", "laa", "lam", "masafa", "meem", "noon", "qaf", "reh", "sad", "seen", "sheen", "tah", "teh", "teh_marbuta", "thal", "theh", "waw", "yeh", "zah", "zain"],
-    "arabic_numbers": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "hadhf", "masafa"],
-    "english_letters": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "delete", "space"],
-    "english_numbers": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "delete", "space"]
+    "arabic_letters": [ "ain", "al", "alef", "beh", "dad", "dal", "feh", "ghain", "hadhf", "hah", "heh", "jeem", "kaf", "khah", "laa", "lam", "masafa", "meem", "noon", "qaf", "reh", "sad", "seen", "sheen", "tah", "teh", "teh_marbuta", "thal", "theh", "waw", "yeh", "zah", "zain"],
+    "arabic_numbers": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","hadhf","masafa"],
+    "english_letters": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","delete","space"],
+    "english_numbers": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","delete","space"]
 }
 
+# mapping
 mapping = {
     "arabic_letters": {
         "ain": "ع", "al": "ال", "alef": "أ", "beh": "ب", "dad": "ض", "dal": "د", "feh": "ف", "ghain": "غ", "hadhf": "حذف", "hah": "ح", 
@@ -57,22 +51,23 @@ mapping = {
         "yeh": "ي", "zah": "ز", "zain": "ز"
     },
     "arabic_numbers": {
-        "0": "٠", "1": "١", "2": "٢", "3": "٣", "4": "٤", "5": "٥", "6": "٦", "7": "٧", "8": "٨", "9": "٩", "hadhf": "حذف", "masafa": "مسافة"
+        "0": "٠", "1": "١", "2": "٢", "3": "٣", "4": "٤", "5": "٥", "6": "٦", "7": "٧", "8": "٨", "9": "٩","hadhf":"حذف","masafa":"مسافة"
     },
     "english_letters": {
         "A": "A", "B": "B", "C": "C", "D": "D", "E": "E", "F": "F", "G": "G", "H": "H", "I": "I", "J": "J", "K": "K", "L": "L", "M": "M", 
-        "N": "N", "O": "O", "P": "P", "Q": "Q", "R": "R", "S": "S", "T": "T", "U": "U", "V": "V", "W": "W", "X": "X", "Y": "Y", "Z": "Z", "delete": "delete", "space": "space"
+        "N": "N", "O": "O", "P": "P", "Q": "Q", "R": "R", "S": "S", "T": "T", "U": "U", "V": "V", "W": "W", "X": "X", "Y": "Y", "Z": "Z","delete":"delete","space":"space"
     },
     "english_numbers": {
-        "0": "0", "1": "1", "2": "2", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9": "9", "delete": "delete", "space": "space"
+        "0": "0", "1": "1", "2": "2", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9": "9","delete":"delete","space":"space"
     }
 }
 
-# landmarks for hand  
+# make landmarks for hand
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(min_detection_confidence=0.7)
 mp_drawing = mp.solutions.drawing_utils
 
+#  the text
 text_field = ""
 
 # BaseModels
@@ -83,7 +78,7 @@ class LanguageRequest(BaseModel):
 class PredictionRequest(BaseModel):    
     frame: str     
 
-# 1- set language 
+#  1- set language 
 @app.post("/set_language/")
 async def set_language(request: LanguageRequest):
     global current_language, current_mode
@@ -110,24 +105,22 @@ async def set_language(request: LanguageRequest):
     current_mode = mode
     return {"message": f"Language set to {language} with {mode} mode"}
 
-# text to speech
+
+AUDIO_FOLDER = "audio_files"
+if not os.path.exists(AUDIO_FOLDER):
+    os.makedirs(AUDIO_FOLDER)
+
+
+# convert text to speech
 def text_to_speech(text, lang):
     try:
         timestamp = int(time.time())
-        output_file = f"output_{timestamp}.mp3"
+        output_file = os.path.join(AUDIO_FOLDER, f"output_{timestamp}.mp3")
         tts = gTTS(text=text, lang=lang)
         tts.save(output_file)
+        os.system(f"start {output_file}")
         
-        bucket = storage.bucket()
-        blob = bucket.blob(f"audio_files/{output_file}")
-        blob.upload_from_filename(output_file)
-        
-        blob.make_public()
-        public_url = blob.public_url
-        
-        os.remove(output_file)
-        
-        return public_url 
+        return output_file  
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error in text-to-speech: {str(e)}")
 
@@ -140,19 +133,30 @@ async def speak_text(text: str = Query(..., description="The text to convert to 
     
     lang = "ar" if language == "ar" else "en"
     
-    audio_url = text_to_speech(text, lang)
+    audio_file = text_to_speech(text, lang)
     
     return {
         "message": "Text-to-speech is played",
         "text": text,
-        "audio_url": audio_url 
+        "audio_file": audio_file 
     }
 
-# 3- predict
+
+# 3- download audio
+@app.get("/download_audio/")
+async def download_audio(file_path: str = Query(..., description="Path to the audio file")):
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="Audio file not found")
+    
+    return FileResponse(file_path, media_type="audio/mp3", filename=os.path.basename(file_path))
+
+
+# 4- predict
 @app.post("/predict/")
 async def predict(request: PredictionRequest):
     global text_field
     
+    # Decode Base64 to numpy array
     frame_data = base64.b64decode(request.frame)
     nparr = np.frombuffer(frame_data, np.uint8)
     frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
@@ -176,7 +180,15 @@ async def predict(request: PredictionRequest):
 
     return {"text": text_field}
 
-# 4- reset
+
+# # 3- text to speech
+# @app.get("/text_to_speech/")
+# async def speak_text():
+#     text_to_speech(text_field)
+#     return {"message": "Text-to-speech is played", "text": text_field}
+    
+
+# 4- reset the text
 @app.post("/reset_text/")
 async def reset_text():
     global text_field
