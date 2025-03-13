a98278206a348a51088c3ccbab65ca9bbd9451b0
Aya Ehab <xxxeeee2468@gmail.com>
2025-03-13 03:09:56 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,9 +16,9 @@ import uuid
 import logging
 
 # Initialize Firebase
-cred = credentials.Certificate("app/voice-ec9bd-firebase-adminsdk-fbsvc-0215fa1324.json")  
+cred = credentials.Certificate("app/voice-ec9bd-firebase-adminsdk-fbsvc-0bef6abfcb.json")  
 firebase_admin.initialize_app(cred, {
-    'storageBucket': 'voice-ec9bd.com'  
+    'storageBucket': 'voice-ec9bd.appspot.com'  
 })
 
 # Function to upload file to Firebase Storage
